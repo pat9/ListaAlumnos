69aa8f86edcaf1625d3bfde08e6d47633797f57d
Joel Chan <joelchan1220@gmail.com>
2017-09-23 19:37:32 -0500

    abc alumnos

---
+++ BO/alumnoBO.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+  public  class alumnoBO
+    {
+        private int id;
+        private string apellidos;
+        private string nombres;
+        private string grado;
+        private string grupo;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Apellidos
+        {
+            get
+            {
+                return apellidos;
+            }
+
+            set
+            {
+                apellidos = value;
+            }
+        }
+
+        public string Nombres
+        {
+            get
+            {
+                return nombres;
+            }
+
+            set
+            {
+                nombres = value;
+            }
+        }
+
+        public string Grado
+        {
+            get
+            {
+                return grado;
+            }
+
+            set
+            {
+                grado = value;
+            }
+        }
+
+        public string Grupo
+        {
+            get
+            {
+                return grupo;
+            }
+
+            set
+            {
+                grupo = value;
+            }
+        }
+    }
+}
---
+++ DAO/alumnosDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using BO;
+
+namespace DAO
+{
+    class alumnosDAO
+    {
+        conexionDAO conectar = new conexionDAO();
+        public int agregar(object agregar)
+        {
+            alumnoBO obejto = (alumnoBO)agregar;
+            SqlCommand dir = new SqlCommand("INSERT INTO alumnos (nombres ,apellidos,grado ,grupo)VALUES (@nom,@apelido,@gra,@gru)");
+           
+            dir.Parameters.Add("@nom", SqlDbType.VarChar).Value = obejto.Nombres;//----
+            dir.Parameters.Add("@apelido", SqlDbType.VarChar).Value = obejto.Apellidos;
+            dir.Parameters.Add("@gra", SqlDbType.VarChar).Value = obejto.Grado;
+            dir.Parameters.Add("@gru", SqlDbType.VarChar).Value = obejto.Grado;
+            dir.CommandType = CommandType.Text;
+        return    conectar.EjecutarComando(dir);
+
+        }
+
+        public int eliminar(object eliminar)
+        {
+            alumnoBO obje = (alumnoBO)eliminar;
+            SqlCommand coma = new SqlCommand("delete from alumno where id=@id");
+            coma.Parameters.Add("id", SqlDbType.Int).Value = obje.Id;
+            coma.CommandType = CommandType.Text;
+            return conectar.EjecutarComando(coma);
+        }
+        public int actualizar(object actualizar)
+        {
+            alumnoBO obejto = (alumnoBO)actualizar;
+            SqlCommand dir = new SqlCommand("update lista set nombres=@nom,apellidos=@apelido,grado=@gra,grupo=@gru where id=@id");
+            dir.Parameters.Add("@nom", SqlDbType.VarChar).Value = obejto.Nombres;//----
+            dir.Parameters.Add("@apelido", SqlDbType.VarChar).Value = obejto.Apellidos;
+            dir.Parameters.Add("@gra", SqlDbType.VarChar).Value = obejto.Grado;
+            dir.Parameters.Add("@gru", SqlDbType.VarChar).Value = obejto.Grado;
+            dir.CommandType = CommandType.Text;
+            return conectar.EjecutarComando(dir);
+        }
+        public DataSet mostrarlista()
+        {
+            SqlCommand comando = new SqlCommand("select * from lista");
+            comando.CommandType = CommandType.Text;
+       return     conectar.EjecutarSentencia(comando);
+        }
+    }
+}
