a805f7e51c86b243b78c91357dd0e6ef69f70e08
Daniel Pat <danielpat6@gmail.com>
2017-09-24 10:32:30 -0500

    Interfaz Alumnos

---
+++ ListaAlumnos/Alumnos.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginaMaestra.Master" AutoEventWireup="true" CodeBehind="Alumnos.aspx.cs" Inherits="ListaAlumnos.Alumnos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div class="row">
+            <asp:TextBox runat="server" ID="txtID" Visible="false"></asp:TextBox>
+            <div class="input-field col s6">
+                <asp:TextBox runat="server"  ID="txtnombres" CssClass="validate"></asp:TextBox>
+                <label for="txtnombres">Nombres</label>
+            </div>
+            <div class="input-field col s6">
+                <asp:TextBox runat="server"  ID="txtapellidos" CssClass="validate"></asp:TextBox>
+                <label for="txtnombres">Apellidos</label>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="input-field col s6">
+                <asp:TextBox runat="server"  ID="txtGrado" CssClass="validate"></asp:TextBox>
+                <label for="txtGrado">Grado</label>
+            </div>
+            <div class="input-field col s6">
+                <asp:TextBox runat="server" ID="txtGrupo" CssClass="validate"></asp:TextBox>
+                <label for="txtGrupo">Grupo</label>
+            </div>
+        </div>
+        <div class="row">
+            <asp:Button CssClass="waves-effect waves-light btn" runat="server" ID="btnAgregar" Text="Agregar" />
+            <asp:Button CssClass="waves-effect waves-light btn" runat="server" ID="btnModificar" Text="Modificar" />
+            <asp:Button CssClass="waves-effect waves-light btn" runat="server" ID="btnEliminar" Text="Eliminar" />
+            
+        </div>
+        <div class="row">
+            <asp:GridView runat="server" ID="gvAlumnos">
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ ListaAlumnos/Alumnos.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ListaAlumnos {
+    
+    
+    public partial class Alumnos {
+        
+        /// <summary>
+        /// Control txtID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtID;
+        
+        /// <summary>
+        /// Control txtnombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombres;
+        
+        /// <summary>
+        /// Control txtapellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtapellidos;
+        
+        /// <summary>
+        /// Control txtGrado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtGrado;
+        
+        /// <summary>
+        /// Control txtGrupo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtGrupo;
+        
+        /// <summary>
+        /// Control btnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregar;
+        
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
+        
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+        
+        /// <summary>
+        /// Control gvAlumnos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvAlumnos;
+    }
+}
