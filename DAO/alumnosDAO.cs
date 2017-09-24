18a889626f8f26f607812bd1fed695fc8bd7c0ba
Joel Chan <joelchan1220@gmail.com>
2017-09-24 17:05:53 -0500

    cambios listo

--- DAO/alumnosDAO.cs
+++ DAO/alumnosDAO.cs
@@ -38,10 +38,11 @@ namespace DAO
         {
             alumnoBO obejto = (alumnoBO)actualizar;
             SqlCommand dir = new SqlCommand("update alumnos set nombres=@nom,apellidos=@apelido,grado=@gra,grupo=@gru where id=@id");
+            dir.Parameters.Add("id", SqlDbType.Int).Value = obejto.Id;
             dir.Parameters.Add("@nom", SqlDbType.VarChar).Value = obejto.Nombres;//----
             dir.Parameters.Add("@apelido", SqlDbType.VarChar).Value = obejto.Apellidos;
             dir.Parameters.Add("@gra", SqlDbType.VarChar).Value = obejto.Grado;
-            dir.Parameters.Add("@gru", SqlDbType.VarChar).Value = obejto.Grado;
+            dir.Parameters.Add("@gru", SqlDbType.VarChar).Value = obejto.Grupo;
             dir.CommandType = CommandType.Text;
             return conectar.EjecutarComando(dir);
         }
